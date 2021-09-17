34af603d97c4f012536457ad6b30c62a3276d69c
Emil Vikström <evikstrm@gmail.com>
2021-09-17 13:28:49 +0200

    Add configure PORT

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,16 @@ config :bladesinthedark, BladesInTheDark.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :bladesinthedark, BladesInTheDarkWeb.Endpoint,
-  http: [port: 4000],
+  http: [
+    port:
+      case System.get_env("PORT") do
+        nil ->
+          4000
+
+        port ->
+          String.to_integer(port)
+      end
+  ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
